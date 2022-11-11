11df1be722e4ca017e5be7b721eb90c88324b9b9
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2022-11-11 19:43:55 -0300

    error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Blog from "./pages/Blog";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 import Us from "./pages/Us";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" exact element={<Home />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/us" element={<Us />} />
         <Route path="*" element={<NotFound />} />
