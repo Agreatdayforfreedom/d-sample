db5da9e840d3cbd41ae255ce2898a5404858d18e
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2022-11-11 19:29:19 -0300

    some more pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Blog from "./pages/Blog";
 import Home from "./pages/Home";
+import Us from "./pages/Us";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<Blog />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/us" element={<Us />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Us.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Us = () => {
+  return (
+    <div>
+      Us
+      <Link to="/">Home</Link>
+    </div>
+  );
+};
+
+export default Us;
